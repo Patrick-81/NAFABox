728c7c1befff82a4976d7fb05af0375c0aee0724
moustickk <31795849+moustickk@users.noreply.github.com>
2017-09-14 16:53:19 +0200

    Update install_other3rdparty_drivers.sh

--- ConfigTinker/sous-programme/install_other3rdparty_drivers.sh
+++ ConfigTinker/sous-programme/install_other3rdparty_drivers.sh
@@ -1,11 +1,20 @@
+################################################
+# Under GPL license
+#     https://www.gnu.org/licenses/gpl.html
+# Authors:	Patrick Dutoit
+# 			Laurent Roge
+# On June 10 2017
+# V0.1
+# script appelé par install_kstars.sh
+################################################
 #!/bin/bash
-######
-# Recherche du répertoire ConfigTinker
-######
+#
 dirinstall=$(find ~ -name ConfigTinker)
-######
-# Installation du driver : atikccd
-######
+#
+####################################
+# Installation du driver : atikccd #
+####################################
+#
 # détection de l'architecture
 case $(uname -p) in
 "armv7l") 
@@ -15,15 +24,18 @@ case $(uname -p) in
     proc="-amd64"
     ;;
 esac
+#
 software="atikccd"
 version="$software""-""1.24"
 file="$version""$proc.deb"
 echo $file
 wget http://download.cloudmakers.eu/$file -P /tmp/
 sudo dpkg -i /tmp/$file
-######
-# Installation du driver : inova
-######
+#
+##################################
+# Installation du driver : inova #
+##################################
+#
 # détection de l'architecture
 case $(uname -p) in
 "armv7l") 
@@ -33,11 +45,20 @@ case $(uname -p) in
     proc="_x86_64-linux-gnu"
     ;;
 esac
+#
 software="indi-inova-ccd-"
 version="$software""-""1.3.0"
 file="$version""$proc.deb"
 echo $file
 wget http://www.inova-ccd.fr/download/E.\ Drivers/Linux/$file -P /tmp/
 sudo dpkg -i /tmp/$file
+#
 sudo apt install -f
+#
+#################
+# fin de script #
+#################
+#
+exit
+
 
