968713b910ec99304712a2ca6464224c4aa9f7f4
moustickk <31795849+moustickk@users.noreply.github.com>
2017-09-14 15:18:59 +0200

    Update install_shortcut.sh

--- ConfigTinker/sous-programme/install_shortcut.sh
+++ ConfigTinker/sous-programme/install_shortcut.sh
@@ -5,17 +5,16 @@
 # 			Laurent Roge
 # On June 10 2017
 # V0.1
+# script appelé par install_conf.sh
 ################################################
 #!/bin/bash
-######
-# Recherche du répertoire ConfigTinker
-######
+#
 dirinstall=$(find ~ -name ConfigTinker)
-######
-source detect_language.sh
-######
-# Créer le raccourci bureau
-######
+#
+####################################################
+source $dirinstall/sous-programme/detect_language.sh
+####################################################
+#
 echo $1 $2
 echo $2
 if [ -n "$2"  ]; then
@@ -29,8 +28,9 @@ then
 else
 	desktop="Bureau"
 fi
-######
+
 fichier=~/$desktop/$AppName.desktop
+
 if [ -f $fichier ]; then
 	if $french
 	then
@@ -40,9 +40,16 @@ if [ -f $fichier ]; then
 	fi
 else
 	AppName=$(echo $1 | sed 's/^./\u&/')
-	cat $dirinstall/template.desktop  | sed -e "s/APP_NAME/$1/g" > /tmp/shortcut1
+	cat $dirinstall/annexe/template.desktop  | sed -e "s/APP_NAME/$1/g" > /tmp/shortcut1
 	cat /tmp/shortcut1  | sed -e "s%APP_EXEC%$AppExec%g" > /tmp/shortcut2
 	cat /tmp/shortcut2 | sed -e "s/NAME/$AppName/g" > ~/$desktop/$AppName.desktop
 	chmod +x ~/$desktop/$AppName.desktop
 fi
+#
+#################
+# fin de script #
+#################
+#
+exit
+
 
