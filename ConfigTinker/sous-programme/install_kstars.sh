63e2864cdcfe6c1a7df93c2b96774d839296cbd2
moustickk <31795849+moustickk@users.noreply.github.com>
2017-09-14 16:58:50 +0200

    Update install_kstars.sh

--- ConfigTinker/sous-programme/install_kstars.sh
+++ ConfigTinker/sous-programme/install_kstars.sh
@@ -5,56 +5,62 @@
 # 			Laurent Roge
 # On June 10 2017
 # V0.1
+# script appelé par update_conf.sh
 ################################################
 #!/bin/bash
-######
-# Recherche du répertoire ConfigTinker
-######
+#
 dirinstall=$(find ~ -name ConfigTinker)
-######
+#
+###################
+# options apt-get #
+###################
+#
 options="--auto-remove --yes -q"
-######
-# detect language
-######
-source detect_language.sh
-######
-# Installation des pré-requis
-######
+####################################################
+source $dirinstall/sous-programme/detect_language.sh
+####################################################
+#
+###############################
+# Installation des pré-requis #
+###############################
+#
 if $french
 then
 	sudo apt-get $options language-pack-kde-fr
 fi
 sudo apt-get $options install gsc
 sudo apt-get $options install libqt5sql5-sqlite
-######
-# Installation du programme : kstars
-#              du serveur : indi
-#              de tous les drivers
-######
+#
+######################################
+# Installation du programme : kstars #
+#              du serveur : indi     #
+#              de tous les drivers   #
+######################################
+#
 sudo apt-get $options install indi-full kstars-bleeding
-######
+#
 # Installation des drivers 3rdparty qui ne sont pas sous forme de dépot
-######
-$dirinstall/install_other3rdparty_drivers.sh
-######
+#
+$dirinstall/sous-programme/install_other3rdparty_drivers.sh
+#
 # Installer le pad amélioré
-######
-$dirinstall/install_pad.sh
-######
+#
+$dirinstall/sous-programme/install_pad.sh
+#
 # Installation du web manager pour indi
-######
-$dirinstall/install_indiwebmanager.sh
-######
+#
+$dirinstall/sous-programme/install_indiwebmanager.sh
+#
 # Création de l'icône sur le bureau
-######
-$dirinstall/install_shortcut.sh kstars
-######
+#
+$dirinstall/sous-programme/install_shortcut.sh kstars
+#
 # Installation du programme de résolution astrométrique
-######
+#
 sudo apt-get $options install astrometry.net
-######
-# Installation des index
-######
-#$dirinstall/install_index.sh
-
-
+#
+#################
+# fin de script #
+#################
+#
+exit
