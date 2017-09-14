cc2de24b7abe86b8a512f97a997a17583bd3089e
moustickk <31795849+moustickk@users.noreply.github.com>
2017-09-14 13:51:28 +0200

    Update go.sh

--- ConfigTinker/go.sh
+++ ConfigTinker/go.sh
@@ -2,13 +2,23 @@
 # Under GPL license
 #     https://www.gnu.org/licenses/gpl.html
 # Authors:	Patrick Dutoit
-# 			Laurent Roge
+# 			    Laurent Roge
 # On June 10 2017
 # V0.1
+# script d'installation principal
 ################################################
 #!/bin/bash
+#set -xv
+sudo apt-get update
+sudo dpkg --configure -a
 dirinstall=$(find ~ -name ConfigTinker)
 cd $dirinstall
-chmod +x ./*
+chmod +x ./*.sh
+#
 ./install_conf.sh
-
+#
+##########################
+# fin du script primaire #
+##########################
+#
+exit
