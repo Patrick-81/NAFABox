e8527a93483ca2cc1c13377f7154248067248485
moustickk <31795849+moustickk@users.noreply.github.com>
2017-09-14 16:38:09 +0200

    Update install_webdavserver.sh

--- ConfigTinker/sous-programme/install_webdavserver.sh
+++ ConfigTinker/sous-programme/install_webdavserver.sh
@@ -5,36 +5,34 @@
 # 			Laurent Roge
 # On June 10 2017
 # V0.1
+# script appelé par install_comsetup.sh
 ################################################
 #!/bin/bash
-######
-# Recherche du répertoire ConfigTinker
-######
+#
 dirinstall=$(find ~ -name ConfigTinker)
-######
-# recupere le user
-######
 moi=$(whoami)
 data=$dirinstall
-######
-# detect language
-######
-source detect_language.sh
-######
-# Installation du serveur webdav
-######
-# Extras nginx
+#
+##################################
+# Installation du serveur webdav #
+##################################
+#
 sudo apt -y install nginx-extras
+#
 # copy of the server file
-cat $data/webdav-server.txt | sed -e "s/MOI/${moi}/g" > /tmp/site-webdav
+#
+cat $data/annexe/webdav-server.txt | sed -e "s/MOI/${moi}/g" > /tmp/site-webdav
 sudo cp /tmp/site-webdav /etc/nginx/sites-available/.
 sudo chown $moi:$moi /etc/nginx/sites-available/site-webdav
 sudo ln -sf /etc/nginx/sites-available/site-webdav /etc/nginx/sites-enabled/site-webdav
+#
 # Restart nginx
 sudo service nginx stop
 sleep 2
 sudo service nginx start
-
-
-
-
+#
+#################
+# fin de script #
+#################
+#
+exit
