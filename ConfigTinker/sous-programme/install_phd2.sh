557be2a737d0d443882100e245b4f72132a01394
moustickk <31795849+moustickk@users.noreply.github.com>
2017-09-14 17:01:20 +0200

    Update install_phd2.sh

--- ConfigTinker/sous-programme/install_phd2.sh
+++ ConfigTinker/sous-programme/install_phd2.sh
@@ -5,23 +5,29 @@
 # 			Laurent Roge
 # On June 10 2017
 # V0.1
+# script appelé par update_conf.sh
 ################################################
 #!/bin/bash
-######
-# Recherche du répertoire ConfigTinker
-######
+#
 dirinstall=$(find ~ -name ConfigTinker)
-######
+#
+###################
+# options apt-get #
+###################
+#
 options="--auto-remove --yes -q"
-######
-# Installation des pré-requis
-######
-######
-# Installation du programme : phd2
-######
+#
+####################################
+# Installation du programme : phd2 #
+####################################
+#
 sudo apt-get $options install phd2
-######
+#
 # Création de l'icône sur le bureau
-######
-$dirinstall/install_shortcut.sh phd2
-
+$dirinstall/sous-programme/install_shortcut.sh phd2
+#
+#################
+# fin de script #
+#################
+#
+exit
