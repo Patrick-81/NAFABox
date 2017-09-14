c92387f8c194dcbbc42effcd57d272cf8ca37fb4
moustickk <31795849+moustickk@users.noreply.github.com>
2017-09-14 17:06:13 +0200

    Update install_libpasastro.sh

--- ConfigTinker/sous-programme/install_libpasastro.sh
+++ ConfigTinker/sous-programme/install_libpasastro.sh
@@ -5,17 +5,27 @@
 # 			Laurent Roge
 # On June 10 2017
 # V0.1
+# script appelé par install_skychart.sh
 ################################################
 #!/bin/bash
-######
-# Recherche du répertoire ConfigTinker
-######
+#
 dirinstall=$(find ~ -name ConfigTinker)
-######
-# Installation de libpasastro
-source $dirinstall/proctype.sh
+#
+###############################
+# Installation de libpasastro #
+###############################
+#
+# détéction de l'architecture
+source $dirinstall/sous-programme/proctype.sh
+#
 software="libpasastro"
 version="version_1.1-19"
 file="libpasastro_1.1-19$proc.deb"
 wget https://sourceforge.net/projects/$software/files/$version/$file -P /tmp/
 sudo dpkg -i /tmp/$file
+#
+#################
+# fin de script #
+#################
+#
+exit
