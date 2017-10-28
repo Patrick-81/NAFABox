4a3cd729cac57c39b6ab532b9bbf7abd5cc39f3e
dragonlost <dragonlost1@hotmail.fr>
2017-10-29 01:24:59 +0200

    add script for fix permision on network manager

---
+++ ConfigTinker/fix_permision_network_manager.sh
@@ -0,0 +1,27 @@
+################################################
+# Under GPL license
+#     https://www.gnu.org/licenses/gpl.html
+# Authors:	Sébastien Durand
+# 			
+#
+# Modified: 
+#
+# On October 29 2017
+# V0.1
+################################################
+#/bin/bash
+######
+
+######
+# Installation du policykit manquant
+######
+
+dir=/etc/polkit-1/localauthority/50-local.d/  #mettre le chemin de polkit local
+
+echo -e '[nm-applet]\nIdentity=unix-group:netdev;unix-group:sudo\nAction=org.freedesktop.NetworkManager.*\nResultAny=yes\nResultInactive=no\nResultActive=yes\n' >> /tmp/org.freedesktop.NetworkManager.pkla
+
+
+sudo cp /tmp/org.freedesktop.NetworkManager.pkla $dir
+sudo rm /tmp/org.freedesktop.NetworkManager.pkla
+sudo adduser $USER sudo
+sudo adduser $USER netdev
