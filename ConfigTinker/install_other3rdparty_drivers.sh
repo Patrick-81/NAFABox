d0675bc1a7b9c23ab6c3e3497d50cf1a15cf0293
dragonlost <dragonlost1@hotmail.fr>
2017-10-29 19:30:24 +0100

    update atik indi driver version

--- ConfigTinker/install_other3rdparty_drivers.sh
+++ ConfigTinker/install_other3rdparty_drivers.sh
@@ -16,7 +16,7 @@ case $(uname -p) in
     ;;
 esac
 software="atikccd"
-version="$software""-""1.25"
+version="$software""-""1.26"
 file="$version""$proc.deb"
 echo $file
 wget http://download.cloudmakers.eu/$file -P /tmp/
