3a8e20bb07301b7011b4896be87b746d9d33ef0b
Patrick-81 <pkdt@sfr.fr>
2018-04-14 10:30:16 +0200

    Modif de l'installation d'astroberry_diy pour généraliser quel que soit la carte

--- ConfigTinker/install_astroberry_diy.sh
+++ ConfigTinker/install_astroberry_diy.sh
@@ -18,9 +18,22 @@
 ######
 dirinstall=$(find ~ -name ConfigTinker)
 ######
+sudo apt install --reinstall lshw
 mkdir -p /home/nafa/Projects
 cd /home/nafa/Projects
-git clone https://github.com/rkaczorek/astroberry-diy
+machine=$(sudo lshw | grep "produit\|product" | grep "Intel")
+if [[ $machine == *"Intel"* ]]
+then
+	echo "GPIO non disponible\n"
+	exit
+fi
+machine=$(sudo lshw | grep "produit\|product" | grep "Raspberry")
+if [[ $machine == *"Raspberry"* ]]
+then 
+	git clone -b https://github.com/rkaczorek/astroberry-diy
+else
+	git clone -b tinkerboard https://github.com/rkaczorek/astroberry-diy
+fi	
 git clone https://github.com/RPi-Distro/RTIMULib
 git clone https://github.com/TinkerBoard/gpio_lib_c 
 
@@ -36,15 +49,19 @@ cd /home/nafa/Projects/RTIMULib/RTIMULib && mkdir -p build && cd build
 sudo rm -rf * && cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. && sudo make install
 
 cd /home/nafa/Projects/astroberry-diy
-patch CMakeLists.txt -i $dirinstall/CMakeLists.patch -o TEMP
-cp -f TEMP CMakeLists.txt
-patch rpi_focus.cpp -i $dirinstall/rpi_focus.patch -o TEMP
-cp -f TEMP rpi_focus.cpp
-patch rpi_brd.cpp -i $dirinstall/rpi_brd.patch -o TEMP
-cp -f TEMP rpi_brd.cpp
+#patch CMakeLists.txt -i $dirinstall/CMakeLists.patch -o TEMP
+#cp -f TEMP CMakeLists.txt
+#patch rpi_focus.cpp -i $dirinstall/rpi_focus.patch -o TEMP
+#cp -f TEMP rpi_focus.cpp
+#patch rpi_brd.cpp -i $dirinstall/rpi_brd.patch -o TEMP
+#cp -f TEMP rpi_brd.cpp
 mkdir -p build && cd build
-sudo rm -rf * && cmake -DCMAKE_INSTALL_PREFIX=/usr -DTINKER=1 -DCMAKE_BUILD_TYPE=Debug .. && sudo make install
-
+if [[ $machine == *"Raspberry"* ]]
+then
+	sudo rm -rf * && cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. && sudo make install
+else
+	sudo rm -rf * && cmake -DCMAKE_INSTALL_PREFIX=/usr -DTINKER=1 -DCMAKE_BUILD_TYPE=Debug .. && sudo make install
+fi
 
 
 
