c9e68b8a27d3f387e32d73af4d13a7988575d198
dragonlost <dragonlost1@hotmail.fr>
2019-02-02 12:39:15 +0100

    add server option

--- Install.sh
+++ Install.sh
@@ -8,14 +8,21 @@
 ################################################
 #!/bin/bash -i
 
+server_choice=$1
+
 if [ -z "$nafabox_path" ]
 then
 	echo "Run first Pre_Install.sh and reload Terminal"
 else
-	sudo apt update
+	sudo apt-get update
 	figlet -k NAFABox
 	echo "================================================="
 	echo "================================================="
+	if [[ $server_choice == "server" ]]
+	then
+		$nafabox_path/install_conf.sh server | tee -a "$nafabox_path/nafabox.log"
+	else
 
-	$nafabox_path/install_conf.sh | tee -a "$nafabox_path/nafabox.log"
+		$nafabox_path/install_conf.sh | tee -a "$nafabox_path/nafabox.log"
+	fi
 fi
