80611869682b853a4a3e407e05f5043dfa27fb2a
dragonlost <dragonlost1@hotmail.fr>
2019-02-04 11:33:07 +0100

    Update install_oacapture.sh

--- ConfigTinker/install_oacapture.sh
+++ ConfigTinker/install_oacapture.sh
@@ -33,7 +33,8 @@ fi
 ######
 # Installation des pré-requis
 #######
-sudo apt-get -y install libv4l-dev libqt4-dev libdc1394-22-dev libcfitsio-dev libudev-dev libtiff-dev qt4-dev-tools qt4-qmake gawk gcc g++ yasm autoconf libtool m4 automake libevent-dev pkg-config libhidapi-dev libpng-dev libsdl-dev libusb-dev libusb-1.0 libftdi-dev libraw1394-11 libgtkmm-2.4-1v5 libglademm-2.4-1v5 libgtkglextmm-x11-1.2-dev libgtkglextmm-x11-1.2 qt5-default
+sudo apt-get -y install qt4-dev-tools qt4-qmake libqt4-dev
+sudo apt-get -y install libv4l-dev libdc1394-22-dev libcfitsio-dev libudev-dev libtiff-dev gawk gcc g++ yasm autoconf libtool m4 automake libevent-dev pkg-config libhidapi-dev libpng-dev libsdl-dev libusb-dev libusb-1.0 libftdi-dev libraw1394-11 libgtkmm-2.4-1v5 libglademm-2.4-1v5 libgtkglextmm-x11-1.2-dev libgtkglextmm-x11-1.2 qt5-default fxload qtbase5-dev
 sudo apt-get -y install libuvc-dev
 # go at home
 cd ~
@@ -43,8 +44,8 @@ then
 	# mise a jour
 	echo "update OACapture git"
 	cd /home/$USER/openastro/
-	sudo make clean
 	sudo make uninstall
+	sudo make clean
 	git pull
 else
 	# installation
