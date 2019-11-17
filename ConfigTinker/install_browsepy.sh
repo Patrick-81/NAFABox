b26d665fcee2eae8d85439550d79bd433d1b079a
dragonlost <dragonlost1@hotmail.fr>
2019-11-17 22:40:24 +0100

    Update install_browsepy.sh
    change process for run browsepy

--- ConfigTinker/install_browsepy.sh
+++ ConfigTinker/install_browsepy.sh
@@ -23,12 +23,36 @@ cd ${dirinstall}
 # s'assurer que que pip est installe
 sudo apt-get -y install python-pip python3-pip python-setuptools python3-setuptools
 # installer browsepy
-sudo pip install browsepy
+pip3 install scandir
+pip3 install backports.shutil_get_terminal_size
+pip3 install browsepy
 # Lancer le serveur au demarrage
 # placer la commande sous .config/autostart
-cat ${dirinstall}/browsepy.desktop | sed -e "s#MOI#${HOME}#g" > /tmp/browsepy.desktop
-mkdir -p ~/.config/autostart/
-sudo cp /tmp/browsepy.desktop $HOME/.config/autostart/browsepy.desktop
+#cat ${dirinstall}/browsepy.desktop | sed -e "s#MOI#${HOME}#g" > /tmp/browsepy.desktop
+#mkdir -p ~/.config/autostart/
+#sudo cp /tmp/browsepy.desktop $HOME/.config/autostart/browsepy.desktop
+dir=/etc/systemd/system   #mettre le chemin du service en memoire
+MOI=${USER}
+
+sudo updatedb     # mettre à jour la base d'indexation
+
+dirbrowsepy="/home/${USER}/.local/bin/browsepy"  #récupérer le chemin du binaire browsepy
+
+echo -e '[UNIT]\nDescription=Browsepy\nAfter=multi-user.target\n\n[Service]\nType=idle\nUser='${MOI}'\nExecStart='${dir_browsepy}' --directory /home/'${MOI}' 0.0.0.0 8180\nRestart=Always\nRestartSec=5\n[Install]\nWantedBy=multi-user.target' >> /tmp/browsepy.service
+
+sudo cp /tmp/browsepy.service ${dir}/
+sudo rm /tmp/browsepy.service
+sudo chmod 644 ${dir}/browsepy.service
+
+######
+# enregistrer/lancer le service
+######
+
+sudo systemctl daemon-reload
+sudo systemctl enable browsepy.service
+sudo systemctl start browsepy.service
+
+
 # fin
 
 
