c78ade3ccf73d14b99d9b6ca5b195cdd119c2e0f
dragonlost <dragonlost1@hotmail.fr>
2019-12-14 14:38:23 +0100

    Update install_setres.sh

--- ConfigTinker/install_setres.sh
+++ ConfigTinker/install_setres.sh
@@ -23,12 +23,7 @@ echo "================================================="
 
 ######
 source ${dirinstall}/detect_language.sh
-if test -d Desktop
-then
-	desktop="Desktop"
-else
-	desktop="Bureau"
-fi
+
 sudo apt-get -y install python-tk python-imaging-tk python3-tk
 cp $dirinstall/setres.py ~/bin/.
 cp $dirinstall/resol.scr ~/.config/.
