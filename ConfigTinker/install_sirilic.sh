e3f262ff4ab55129e9bec0ea51a32392045ac852
dragonlost <dragonlost1@hotmail.fr>
2020-07-30 13:03:21 +0200

    change sirilic install process
    use .deb because pyinstaller is not installable on arm. Need check new instal process

--- ConfigTinker/install_sirilic.sh
+++ ConfigTinker/install_sirilic.sh
@@ -25,29 +25,32 @@ echo "================================================="
 #######
 
 sudo apt-get -y install python3 python3-pip imagemagick python3-wx*
-sudo pip3 install pyinstaller
+# sudo pip3 install pyinstaller
 
 ######
 # Clonage de sirilic
 ######
-mkdir -p /home/$USER/bin/
-cd /home/$USER/bin/
-git clone https://gitlab.com/free-astro/sirilic.git
+# mkdir -p /home/$USER/bin/
+# cd /home/$USER/bin/
+# git clone https://gitlab.com/free-astro/sirilic.git
 ######
 # Installation
 ######
-cd /home/$USER/bin/sirilic/sirilic/package
-./MakePackage.sh
-sudo cp dist/* /usr/local/bin/.
-cd ..
+cd /tmp/
+wget https://gitlab.com/free-astro/sirilic/uploads/56cc1649b374542cfc0f590ee1319799/python3-sirilic_1.12.4-1_all.deb
+sudo ap-get -y install /tmp/python3-sirilic_1.12.4-1_all.deb
+# cd /home/$USER/bin/sirilic/sirilic/package
+# ./MakePackage.sh
+# sudo cp dist/* /usr/local/bin/.
+# cd ..
 ######
 # Installer un mini UI pour lancer Sirilic ou Sirilot ou SirilRemote
 ######
-cp ${dirinstall}/SirilicMiniUI.sh /home/$USER/bin/.
-chmod +x /home/$USER/bin/SirilicMiniUI.sh
-sudo ln -sf /home/$USER/bin/SirilicMiniUI.sh /usr/local/bin/SirilicMiniUI
-convert cp-neg.ico cp-neg.png
-sudo cp  cp-neg.png /usr/share/pixmaps/Sirilic.png
-${dirinstall}/install_shortcut.sh APPNAME='sirilic'  APPEXEC='SirilicMiniUI' TERMINAL='false' OPTION='0'
+# cp ${dirinstall}/SirilicMiniUI.sh /home/$USER/bin/.
+# chmod +x /home/$USER/bin/SirilicMiniUI.sh
+# sudo ln -sf /home/$USER/bin/SirilicMiniUI.sh /usr/local/bin/SirilicMiniUI
+# convert cp-neg.ico cp-neg.png
+# sudo cp  cp-neg.png /usr/share/pixmaps/Sirilic.png
+# ${dirinstall}/install_shortcut.sh APPNAME='sirilic'  APPEXEC='SirilicMiniUI' TERMINAL='false' OPTION='0'
 figlet -k Fin Install Sirilic
 
