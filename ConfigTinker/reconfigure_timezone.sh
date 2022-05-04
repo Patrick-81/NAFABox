373f69c27195d0653460676f0407771659feb74f
dragonlost <dragonlost1@hotmail.fr>
2022-05-04 23:37:41 +0200

    Create reconfigure_timezone.sh

---
+++ ConfigTinker/reconfigure_timezone.sh
@@ -0,0 +1,10 @@
+################################################
+# Under GPL license
+#     https://www.gnu.org/licenses/gpl.html
+# Authors:	Sébastien Durand
+# 			
+# On Feb 12 2019
+# V0.1
+################################################
+#!/bin/bash -i
+
