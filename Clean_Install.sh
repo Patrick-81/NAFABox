f205434675ae3f4c7ef0605aa1aa51503c95e4e2
dragonlost <dragonlost1@hotmail.fr>
2022-05-29 23:28:27 +0200

    Update Clean_Install.sh

--- Clean_Install.sh
+++ Clean_Install.sh
@@ -18,5 +18,13 @@ else
 	echo "############################"
         echo "##   Clean Installation   ##"
         echo "############################" 
+	
+	sudo apt-get -y purge caffeine mc mc-data geany gnome-builder meld codium gimp pinta thunderbird putty* celluloid kazam terminator *xterm* filezilla* hexchat libreoffice* 
+	sudo apt-get -y autoremove
+	
+	sudo rm -i /etc/apt/sources.list.d/archive_uri-http_paulcarroty_gitlab_io_vscodium-deb-rpm-repo_debs_-jammy.list 
+	sudo rm -i /etc/apt/sources.list.d/saiarcot895-ubuntu-chromium-dev-jammy.list 
+	
+	sudo apt-get update
 
 fi
