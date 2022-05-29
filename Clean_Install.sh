ffe0983a48cd54b125f059d29f10ae96ca0d55dc
dragonlost <dragonlost1@hotmail.fr>
2022-05-29 23:20:25 +0200

    Create Clean_Install.sh

---
+++ Clean_Install.sh
@@ -0,0 +1,18 @@
+################################################
+# Under GPL license
+#     https://www.gnu.org/licenses/gpl.html
+# Authors:	Sébastien Durand
+# 			
+# On Sept 02 2018
+# V0.1
+################################################
+#!/bin/bash -i
+
+server_choice=$1
+
+if [[ -z "$nafabox_path" ]]
+then
+	echo "Run first Pre_Install.sh and reload Terminal"
+else
+
+fi
