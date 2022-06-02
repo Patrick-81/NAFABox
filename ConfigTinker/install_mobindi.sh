08ad78472cc835d8affdfb93b15939deef2cae0c
dragonlost <dragonlost1@hotmail.fr>
2022-06-02 23:11:24 +0200

    Update install_mobindi.sh

--- ConfigTinker/install_mobindi.sh
+++ ConfigTinker/install_mobindi.sh
@@ -36,7 +36,6 @@ if [ $? -eq 0 ]; then
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt-get install -y nodejs
     sudo apt-get install -y npm
-    npm install
 
     #######
     # clonage mobindi dans ~/bin
