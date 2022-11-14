e988e2ba5de41199f05757e1a9055c5ae57f578e
dragonlost <dragonlost1@hotmail.fr>
2022-11-14 19:29:59 +0100

    add remove software for armbian

--- Clean_Install.sh
+++ Clean_Install.sh
@@ -19,7 +19,7 @@ else
         echo "##   Clean Installation   ##"
         echo "############################" 
 	
-	sudo apt-get -y purge caffeine mc mc-data geany gnome-builder meld codium gimp pinta thunderbird putty* celluloid kazam terminator *xterm* filezilla* hexchat libreoffice* 
+	sudo apt-get -y purge caffeine mc mc-data geany gnome-builder meld codium gimp pinta thunderbird putty* celluloid kazam terminator *xterm* filezilla* hexchat libreoffice* code bleachbit pidgin telegram-desktop telegram-purple 
 	sudo apt-get -y autoremove
 	
 	sudo rm -i /etc/apt/sources.list.d/archive_uri-http_paulcarroty_gitlab_io_vscodium-deb-rpm-repo_debs_-jammy.list 
