badf4677c2b4d7d4da6817b3183abca3c72d5243
dragonlost <dragonlost1@hotmail.fr>
2023-03-21 15:13:10 +0100

    add new utility script

---
+++ utility/Clean_Armbian_Install.sh
@@ -0,0 +1,30 @@
+################################################
+# Under GPL license
+#     https://www.gnu.org/licenses/gpl.html
+# Authors:	Sébastien Durand
+# 			
+# On Sept 02 2018
+# V0.1
+################################################
+#!/bin/bash -i
+
+server_choice=$1
+
+if [[ -z "$nafabox_path" ]]
+then
+	echo "Run first Pre_Install.sh and reload Terminal"
+else
+
+	echo "############################"
+        echo "##   Clean Installation   ##"
+        echo "############################" 
+	
+	sudo apt-get -y purge caffeine mc mc-data geany gnome-builder meld codium gimp pinta thunderbird putty* celluloid kazam terminator *xterm* filezilla* hexchat libreoffice* code bleachbit pidgin telegram-desktop telegram-purple 
+	sudo apt-get -y autoremove
+	
+	sudo rm -i /etc/apt/sources.list.d/archive_uri-http_paulcarroty_gitlab_io_vscodium-deb-rpm-repo_debs_-jammy.list 
+	sudo rm -i /etc/apt/sources.list.d/saiarcot895-ubuntu-chromium-dev-jammy.list 
+	
+	sudo apt-get update
+
+fi
