b484f1f2343a6210faedd0825bbea441290d932e
dragonlost <dragonlost1@hotmail.fr>
2023-10-31 00:01:11 +0100

    Update xubuntu_armbian_install.sh
    add keyfile config and power saver status and managed up/down

--- utility/xubuntu_armbian_install.sh
+++ utility/xubuntu_armbian_install.sh
@@ -43,6 +43,10 @@ if [[ $1 == "rpi" ]]; then
   sudo systemctl stop systemd-networkd-wait-online.service
   sudo systemctl disable systemd-networkd-wait-online.service
   sudo rm /etc/network/interfaces.d/99-add-config.conf
-  sudo printf "# The loopback network interface\nauto lo\niface lo inet loopback\n\n# The primary network interface\nauto eth0\niface eth0 inet dhcp\n" > /tmp/99-add-config.conf
+  printf "# The loopback network interface\nauto lo\niface lo inet loopback\n\n# The primary network interface\nauto eth0\niface eth0 inet dhcp\n" > /tmp/99-add-config.conf
   sudo mv /tmp/99-add-config.conf /etc/network/interfaces.d/99-add-config.conf
+  sudo sed -i 's/wifi.powersave = 3/wifi.powersave = 2/g' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+  sudo sed -i 's/managed=false/managed=true/g' /etc/NetworkManager/NetworkManager.conf
+  printf "[keyfile]\nunmanaged-devices=*,except:type:wifi,except:type:wwan,except:type:ethernet\n" > /tmp/99-keyfile.conf
+  sudo mv /tmp/99-keyfile.conf /etc/NetworkManager/conf.d/99-keyfile.conf
 fi
